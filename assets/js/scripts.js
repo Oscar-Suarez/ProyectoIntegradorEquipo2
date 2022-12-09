6a74c3043cc80415ea595f344c225351b7ac5552
WalleMontero <montero@walle.mx>
2022-12-09 13:33:05 -0600

    Se agrego mas roductos a products-page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -514,6 +514,28 @@ const closeSesion = document.querySelector("#CloseSesion");
     })
 }
 
+let items = document.querySelectorAll('#carouselMultiple .carousel-item')
+
+
+items.forEach((el) => {
+    
+    var slides = items.length;
+    let next = el.nextElementSibling;
+    for (var i = 1; i < slides; i++) {
+        if (!next) {
+            next = items[0]
+        }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+document.querySelector('#carouselMultiple').onresize = (event) => {
+    if(screen.width < 576){
+        console.log("yes");
+    }
+};
 
 
 
