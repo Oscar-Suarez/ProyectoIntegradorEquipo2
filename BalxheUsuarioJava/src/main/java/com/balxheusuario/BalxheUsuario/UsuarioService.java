db454aae3176c689acc0b1612ff62c1a3d97172c
OscarDev <oscar.suarezwgd@gmail.com>
2022-12-22 13:22:12 -0600

    Se actualizó la base de datos y se corrigieron errores. La base de datos en eclipse funciona optimamente

--- BalxheUsuarioJava/src/main/java/com/balxheusuario/BalxheUsuario/UsuarioService.java
+++ BalxheUsuarioJava/src/main/java/com/balxheusuario/BalxheUsuario/UsuarioService.java
@@ -1,7 +1,6 @@
 package com.balxheusuario.BalxheUsuario;
-
 import java.util.List;
-
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 @Service
@@ -17,20 +16,65 @@ public class UsuarioService {
 	
 
 	public List<Usuario> getUsuarios() {
-		// TODO Auto-generated method stub
 		return usuarioRepository.findAll();
+		
 	}
+	// Metodo GET para un usuario por id
+
 	public Usuario getUsuario(Long idUsuario) {
 
-        return null;
-    }
-	public void deleteUsuario(Long idUsuario) {
-		// TODO Auto-generated method stub
-		
+		return usuarioRepository.findById(idUsuario)
+				.orElseThrow(() -> new IllegalStateException("El usuario con el id solicitado no existe"));
 	}
-	public void addUsuario(Usuario usuario) {
-		// TODO Auto-generated method stub
-		
+	
+	
+	//Metodo DELETE para un usuario por id
+	public void deleteUsuario(Long idUsuario) {
+		if(usuarioRepository.existsById(idUsuario)) { //primero reviso que el user exista
+			usuarioRepository.deleteById(idUsuario); //si existe, lo borro
+		}else { //si recibo algo diferente a un numero, disparo la excepcion
+			throw new IllegalStateException("El usuario con el id solicitado no existe, revisa que estes ingresando un valor numérico");
+		}
+	}
+	
+	
+	//Metodo POST para agregar un usuario
+	public void addUsuario(Usuario user) { //guardando entidades del tipo usuario
+		Optional<Usuario> userByName = usuarioRepository.findByUsername(user.getNombreUsuario());//sacando el valor de mi BD y lo pongo en una variable temporal (como en el foreach)
+		if (userByName.isPresent()) { //evaluo si ese dato que esta en esa variable existe en mi BD
+			throw new IllegalStateException("El usuario con el nombre que escribiste ya existe");
+		}
+		usuarioRepository.save(user);}
+
+
+	//Metodo UPDATE para actualizar contrasenias de usuarios
+	public void updateUsuario(Long idUsuario, String contraseniaActual, String nuevaContrasenia) {
+		//primero evaluo si mi usuario existe
+		if(!usuarioRepository.existsById(idUsuario)) {
+			//si no existe, arroja una excepcion
+			throw new IllegalStateException("El usuario no existe");
+		}
+			//cuando el existsById arroja un true (que si existe en la BD), saco el valor y lo pongo en una variable temporal llamada user
+			Usuario user = usuarioRepository.getById(idUsuario); //esta es la variable
+			//despues que las dos contrasenias no sean nulas
+			if(nuevaContrasenia == null || contraseniaActual == null) {
+				//si son nulas, mando mi excepcion
+				throw new IllegalStateException("Constrasenias nulas");
+			}
+			//evaluo que las contrasenias no sean iguales (que la nueva no sea igual a la anterior)
+			if (contraseniaActual.equals(user.getContrasenia())) {
+				//si las constrasenias son diferentes
+				if (!nuevaContrasenia.equals(user.getContrasenia())) {
+					//seteo o actualizo con el setter la nueva contrasenia
+					user.setContrasenia(nuevaContrasenia);
+					//guardo al usuario en su BD con la nueva contrasenia
+					usuarioRepository.save(user);
+				}else {
+					throw new IllegalStateException("La nueva contrasenia es igual a la actual");
+				}
+			}else {
+				throw new IllegalStateException("Contrasea actual incorrecta");
+		}
 	}
 	
 	
